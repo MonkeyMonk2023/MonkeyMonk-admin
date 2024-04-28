4f93075a38d96ebf90c1066f5fbb12fb7afd8052
Mak <Mak>
2024-04-28 21:43:21 +0530

    router tests

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <>
-    <HashRouter basename='/MonkeyMonk-admin'>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/*" element={<DashboardLayout />} />
