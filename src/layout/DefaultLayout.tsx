4e7adeee5b23b46ef8700f573355244788ab8dff
Mak <Mak>
2024-04-28 23:08:13 +0530

    router tests

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -12,50 +12,53 @@ const DefaultLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [featurePlaceFormOpen, setFeaturePlaceFormOpen] = useState(false);
 
   const { user } = UserAuth();
-  // if (!user) {
-    return (
-      <div className="dark:bg-boxdark-2 dark:text-bodydark">
-        <div
-          className={`flex h-screen overflow-hidden ${(blogFormOpen || featurePlaceFormOpen) && 'opacity-30'}`}
-        >
-          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-            <Header
-              sidebarOpen={sidebarOpen}
-              setSidebarOpen={setSidebarOpen}
-              blogFormOpen={blogFormOpen}
-              setBlogFormOpen={setBlogFormOpen}
-              featurePlaceFormOpen={featurePlaceFormOpen}
-              setFeaturePlaceFormOpen={setFeaturePlaceFormOpen}
-            />
-            <main className="z-10">
-              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                {children}
-              </div>
-            </main>
-          </div>
-        </div>
-        {
-          <div className="absolute top-0 justify-center items-center mx-auto w-full h-screen overflow-scroll opacity-100">
-            {blogFormOpen && (
-              <PostBlog
-                blogFormOpen={blogFormOpen}
-                setBlogFormOpen={setBlogFormOpen}
-              />
-            )}
-            {featurePlaceFormOpen && (
-              <PostSuggestion
-                featurePlaceFormOpen={featurePlaceFormOpen}
-                setFeaturePlaceFormOpen={setFeaturePlaceFormOpen}
-              />
-            )}
-          </div>
-        }
-      </div>
-//     );
-//   } else {
-//     return <Navigate to={process.env.PUBLIC_URL + '/login'} />;
-//   }
-)};
+  return(
+    <h1>hey</h1>
+  )
+  // if (user) {
+  //   return (
+  //     <div className="dark:bg-boxdark-2 dark:text-bodydark">
+  //       <div
+  //         className={`flex h-screen overflow-hidden ${(blogFormOpen || featurePlaceFormOpen) && 'opacity-30'}`}
+  //       >
+  //         <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+  //         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+  //           <Header
+  //             sidebarOpen={sidebarOpen}
+  //             setSidebarOpen={setSidebarOpen}
+  //             blogFormOpen={blogFormOpen}
+  //             setBlogFormOpen={setBlogFormOpen}
+  //             featurePlaceFormOpen={featurePlaceFormOpen}
+  //             setFeaturePlaceFormOpen={setFeaturePlaceFormOpen}
+  //           />
+  //           <main className="z-10">
+  //             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+  //               {children}
+  //             </div>
+  //           </main>
+  //         </div>
+  //       </div>
+  //       {
+  //         <div className="absolute top-0 justify-center items-center mx-auto w-full h-screen overflow-scroll opacity-100">
+  //           {blogFormOpen && (
+  //             <PostBlog
+  //               blogFormOpen={blogFormOpen}
+  //               setBlogFormOpen={setBlogFormOpen}
+  //             />
+  //           )}
+  //           {featurePlaceFormOpen && (
+  //             <PostSuggestion
+  //               featurePlaceFormOpen={featurePlaceFormOpen}
+  //               setFeaturePlaceFormOpen={setFeaturePlaceFormOpen}
+  //             />
+  //           )}
+  //         </div>
+  //       }
+  //     </div>
+  //   );
+  // } else {
+  //   return <Navigate to={process.env.PUBLIC_URL + '/login'} />;
+  // }
+};
 
 export default DefaultLayout;
