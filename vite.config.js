3465634a4801d318633f44569a031a3e6eb7a68d
Mak <Mak>
2024-04-28 17:17:16 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/repo",
+  base: "/MonkeyMonk-admin",
   plugins: [react()],
   server: {
     host: true,
